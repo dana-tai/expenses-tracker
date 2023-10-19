6e7dede5f2271564f0a9a01dc0a5f97697a9ae0f
Kalvin Tang <kaltang@kalvins-mbp.mynetworksettings.com>
2023-10-19 16:23:40 -0400

    added features

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 const NavBar = (props) => {
 
     return (
-        <div className='d-flex justify-content-between align-items-center border p-3 mb-4'>
+        <div className='d-flex justify-content-between align-items-center border px-5 py-3 mb-4'>
             <h1>Expense Tracker</h1>
-            <Link to={"/"}>Home</Link>
+            <h4><Link to={"/"}>Home</Link></h4>
         </div>
     )
 }
